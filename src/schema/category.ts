1181659a7bbbd5d1424b7ee253c455ba6c5ce3cb
rayhan889 <raydev1604@gmail.com>
2024-04-29 08:45:12 +0700

    update product and category schema

--- src/schema/category.ts
+++ src/schema/category.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
+import { ProductSchema } from "./product";
 
 export const CategorySchema = z.object({
   id: z.string(),
   name: z.string().min(2),
+  products: z.array(ProductSchema).nullable(),
 });
--- src/schema/product.ts
+++ src/schema/product.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
+import { CategorySchema } from "./category";
 
 export const ProductSchema = z.object({
   id: z.string(),
   name: z.string().min(2),
   price: z.number().min(3),
   description: z.string().min(3),
-  image: z.string(),
+  image: z.string().nullable(),
+  categories: z.array(CategorySchema).nullable(),
 });
