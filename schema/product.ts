7091dbc1d8b05b3693432f90b8a9b2686386a925
rayhan889 <raydev1604@gmail.com>
2024-04-29 08:20:28 +0700

    change naming file for schemas

--- schema/product.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const ProductSchema = z.object({
-  id: z.string(),
-  name: z.string().min(2),
-  price: z.number().min(3),
-  description: z.string().min(3),
-  image: z.string(),
-});
