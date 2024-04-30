4c9a74ae2db8e43b5392d7f4166efb088b8d41bd
rayhan889 <raydev1604@gmail.com>
2024-04-30 16:45:59 +0700

    remove category schema

--- src/schema/category.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { ProductSchema } from "./product";
-
-export const CategorySchema = z.object({
-  id: z.string(),
-  name: z.string().min(2),
-  products: z.array(ProductSchema).nullable(),
-});
