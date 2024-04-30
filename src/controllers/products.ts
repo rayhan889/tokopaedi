81eeeb68c04882092295af8701e96ab6bbe507c7
rayhan889 <raydev1604@gmail.com>
2024-04-30 16:46:35 +0700

    conf product controller & route

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import type { RequestHandler } from "express";
 import { Request, Response } from "express";
+import { Product } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -9,33 +10,66 @@ export const getAllProducts: RequestHandler = async (
   res: Response
 ) => {
   try {
-    const products = await prisma.product.findMany();
+    const products: Product[] = await prisma.product.findMany({
+      include: { categories: { select: { name: true } } },
+    });
     res.status(200).send(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export const getProduct: RequestHandler = async (
+export const getSingleProduct: RequestHandler = (
   req: Request,
   res: Response
 ) => {
-  try {
-    const product = await prisma.product.findUnique({
-      where: { id: req.params.id },
-    });
-    if (product === null || !product) {
-      res.status(404).json({ message: "Cannot find product" });
-    }
-    res.status(200).send(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  res.send(res.product);
 };
 
 export const createProduct: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  res.json({ message: "Product Created!", data: req.body });
+  const { name, price, description, image, categories } = req.body;
+
+  try {
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+        description,
+        image,
+        categories: {
+          connectOrCreate: categories.map((category: string) => {
+            return {
+              where: { name: category },
+              create: { name: category },
+            };
+          }),
+        },
+      },
+    });
+
+    res.send({ message: "Product created!", data: product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProduct: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { name, price, description, image, categories } = req.body;
+  if (name ?? price ?? description ?? image ?? categories) {
+    res.product = {
+      ...res.product,
+      name,
+      price,
+      description,
+      image,
+      categories,
+    };
+  }
+  res.send(res.product);
 };
--- src/routes/api/products.ts
+++ src/routes/api/products.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import {
   getAllProducts,
-  getProduct,
   createProduct,
+  getSingleProduct,
+  updateProduct,
 } from "../../controllers/products";
 import { validateShcema } from "../../middleware/validationSchema";
-import { ProductSchema } from "../../schema/product";
+import { getProduct } from "../../middleware/getProduct";
+import { CreateProductSchema } from "../../schema/product";
 
 export default (router: express.Router) => {
   router.get("/", getAllProducts);
-  router.get("/:id", getProduct);
-  router.post("/", validateShcema(ProductSchema), createProduct);
+  router.get("/:id", getProduct, getSingleProduct);
+  router.post("/", validateShcema(CreateProductSchema), createProduct);
+  router.patch(
+    "/:id",
+    getProduct,
+    validateShcema(CreateProductSchema),
+    updateProduct
+  );
 };
