452fc0be3e3dac1ca7e359a7063a3058c6251167
rayhan889 <raydev1604@gmail.com>
2024-05-02 21:31:59 +0700

    config api path with api in front of it

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/products", routes());
+app.use("/api/products", routes());
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
