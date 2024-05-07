b3db7f4299735dc0f22805ad1dcb3707e1415c9e
rayhan889 <raydev1604@gmail.com>
2024-05-07 23:51:05 +0700

    verify jwt token

---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
+
+export interface UserRequest extends Request {
+  user: string | JwtPayload;
+}
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN,
+    (error: VerifyErrors, decoded: JwtPayload) => {
+      if (error) return res.status(403).json({ message: "Invalid token!" });
+      (req as UserRequest).user = decoded;
+      next();
+    }
+  );
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,13 @@ import products from "./api/products";
 import discounts from "./api/discounts";
 import orders from "./api/orders";
 import auth from "./api/auth";
+import { verifyToken } from "../middleware/verifyToken";
 
 const router = express.Router();
 
-router.use("/products", products);
-router.use("/discounts", discounts);
-router.use("/orders", orders);
 router.use("/auth", auth);
+router.use("/products", verifyToken, products);
+router.use("/discounts", verifyToken, discounts);
+router.use("/orders", verifyToken, orders);
 
 export default router;
