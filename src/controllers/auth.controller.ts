4ed936a783ad4b13e88db4f27b46782d295be63b
rayhan889 <raydev1604@gmail.com>
2024-05-07 15:33:30 +0700

    getting started w/ auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,52 @@
+require("dotenv").config();
+
 import { PrismaClient } from "@prisma/client";
 import type { RequestHandler } from "express";
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { LoginSchemaType } from "../schema/user";
 
 const prisma = new PrismaClient();
 
 export const handleAuth: RequestHandler = async (
   req: Request,
   res: Response
-) => {};
+) => {
+  const { email, password }: LoginSchemaType = req.body;
+
+  const foundUser = await prisma.user.findUnique({
+    where: { email },
+    select: {
+      email: true,
+      username: true,
+      password: true,
+    },
+  });
+
+  if (!foundUser) return res.sendStatus(409);
+
+  const matchPassword = await bcrypt.compare(password, foundUser.password);
+
+  if (matchPassword) {
+    const accessToken = await jwt.sign(
+      {
+        userInfo: {
+          username: foundUser.username,
+          email: foundUser.email,
+        },
+      },
+      process.env.ACCESS_TOKEN,
+      {
+        expiresIn: "30s",
+      }
+    );
+    const refreshToken = await jwt.sign(
+      { email: foundUser.email },
+      process.env.REFRESH_TOKEN,
+      {
+        expiresIn: "1d",
+      }
+    );
+  }
+};
--- src/routes/api/auth/auth.ts
+++ src/routes/api/auth/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { handleAuth } from "../../../controllers/auth.controller";
 import { validateShcema } from "../../../middleware/validationSchema";
+import { LoginSchema } from "../../../schema/user";
 
 const router = express.Router();
 
-router.post("/", handleAuth);
+router.post("/", validateShcema(LoginSchema), handleAuth);
 
 export default router;
