64f386cd7d25ab4b1f39fc5e46515ef09542be5e
rayhan889 <raydev1604@gmail.com>
2024-05-08 13:43:31 +0700

    add cookie parser package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import router from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api", router);
