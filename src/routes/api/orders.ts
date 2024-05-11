b5a00def753ec33267f652a6254c6ef0cfee1ddc
rayhan889 <raydev1604@gmail.com>
2024-05-11 19:10:32 +0700

    enable mdw

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -5,12 +5,13 @@ import {
   createOrder,
   addProductToCart,
 } from "../../controllers/order.controller";
+import { verifyToken } from "../../middleware/verifyToken";
 
 const router = express.Router();
 
 router.get("/", getAllOrders);
 router.get("/:id", getOrder);
-router.post("/cart", addProductToCart);
+router.post("/cart", verifyToken, addProductToCart);
 router.post("/", createOrder);
 
 export default router;
--- src/routes/api/products.ts
+++ src/routes/api/products.ts
@@ -8,10 +8,11 @@ import {
 } from "../../controllers/product.controller";
 import { validateShcema } from "../../middleware/validationSchema";
 import { CreateProductSchema } from "../../schema/product";
+import { verifyToken } from "../../middleware/verifyToken";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", verifyToken, getAllProducts);
 router.get("/:id", getProduct);
 router.post("/", validateShcema(CreateProductSchema), createProduct);
 router.patch("/:id", updateProduct);
