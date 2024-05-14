f0cb89869e751192d5622e03871ce4c9acd7b8fc
rayhan889 <raydev1604@gmail.com>
2024-05-15 01:35:40 +0700

    conf route for order

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -10,10 +10,10 @@ import { verifyToken } from "../../middleware/verifyToken";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getAllItemsAtCart);
+router.get("/cart", verifyToken, getAllItemsAtCart);
 router.get("/:id", getOrder);
 router.post("/cart", verifyToken, addProductToCart);
-router.post("/", createOrder);
+router.post("/", verifyToken, createOrder);
 router.patch("/cart/:id", verifyToken, updateProductAtCart);
 
 export default router;
